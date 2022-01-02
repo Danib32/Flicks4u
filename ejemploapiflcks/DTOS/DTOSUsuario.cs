b518824c69524b5d730377a834a60a5304febb57
Danib32 <bustosd8@gmail.com>
2022-01-02 11:49:47 -0500

    Agregar archivos de proyecto.

---
+++ ejemploapiflcks/DTOS/DTOSUsuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DTOS
+{
+    public class DTOSUsuario
+    {
+        public DTOSUsuario(int id, String contra, string correo)
+        {
+            IDusuario = id;
+            Contrasenia = contra;
+            CorreoElectronico = correo;
+        }
+        public int IDusuario { get; set; }
+        public string Contrasenia { get; set; }
+
+        public string CorreoElectronico { get; set; }
+    }
+}
